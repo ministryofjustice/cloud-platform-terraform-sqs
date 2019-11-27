2ca0d47f2ce4e7f1c78cd2ae22086f67c750c7cf
vijay-veeranki-moj <vijay.veeranki@digital.justice.gov.uk>
2019-11-27 13:34:23 +0000

    Update sqs module to version 4.0 in example

--- example/sqs.tf
+++ example/sqs.tf
@@ -1,5 +1,5 @@
 module "example_sqs" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=3.5"
+  source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=4.0"
 
   environment-name       = "test"
   team_name              = "cp"
