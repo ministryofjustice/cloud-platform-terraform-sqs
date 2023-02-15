fa7de4f1c00f23c395e73cb9af920cb1ebff951e
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2023-02-15 16:10:25 +0000

    Upgrade terraform version to >= 1.2.5

--- versions.tf
+++ versions.tf
@@ -1,12 +1,12 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = ">= 4.27.0"
     }
     random = {
       source = "hashicorp/random"
     }
   }
-  required_version = ">= 0.14"
+  required_version = ">= 1.2.5"
 }
