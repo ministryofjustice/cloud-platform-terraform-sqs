149687282d24d526e672394b61d948cb23087268
carrb-moj <122389039+carrb-moj@users.noreply.github.com>
2023-08-14 10:17:33 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -120,7 +120,7 @@ locals {
 }
 
 resource "aws_sqs_queue" "terraform_queue" {
-  name                              = "${var.team_name}-${var.environment-name}-local.queue_suffix"
+  name                              = "${var.team_name}-${var.environment-name}-${local.queue_suffix}"
   visibility_timeout_seconds        = var.visibility_timeout_seconds
   message_retention_seconds         = var.message_retention_seconds
   max_message_size                  = var.max_message_size
